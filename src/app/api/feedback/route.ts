a2dd77cc5ae402c2045dd2a237eb845e402691d7
Shivajeet Rai <rai.shivajeet@gmail.com>
2023-08-09 15:00:14 +0100

    feedback api endpoint

---
+++ src/app/api/feedback/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+type Feedback = {
+    name?: string,
+    email?: string,
+    message?: string,
+}
+
+export async function POST(request:Request) {
+    const data: Feedback = await request.json()
+    console.log('data: ', data)
+
+    const {name, email, message } = data
+    return NextResponse.json({ name, email, message })
+}
+
+/*
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url)
+    const obj = Object.fromEntries(searchParams.entries())
+    return NextResponse.json(obj)
+}
+*/
